500541ef1c81ae24a15ae2c16d6e689d49ab4094
Ben Harling <benjamin.harling@hotmail.com>
2015-01-11 11:52:52 +0000

    setting up debug media servin

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+print BASE_DIR
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -84,3 +86,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
