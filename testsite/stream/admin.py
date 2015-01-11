9bd889b30c2373c96c9b25f7feffc694ca297b63
Ben Harling <benjamin.harling@hotmail.com>
2015-01-11 12:36:21 +0000

    allow deletions of items at the stream level

--- testsite/stream/admin.py
+++ testsite/stream/admin.py
@@ -4,6 +4,25 @@ from models import Stream
 
 
 class StreamAdmin(admin.ModelAdmin):
-    list_display = ['user', 'created_at', 'tweet', 'photo']
+    list_display = ['user', 'created_at', 'tweet', 'photo', 'deleted']
+    actions=['delete_items', 'restore_items']
+    
+    def delete_items(self, request, queryset):
+        for item in queryset:
+            if item.photo:
+                item.photo.deleted = True
+            if item.tweet:
+                item.tweet.deleted = True
+            item.save()
+    delete_items.short_description = "Delete selected streams"
+    
+    def restore_items(self, request, queryset):
+        for item in queryset:
+            if item.photo:
+                item.photo.deleted = False
+            if item.tweet:
+                item.tweet.deleted = False
+            item.save()
+    restore_items.short_description = "Restore selected streams"
 
 admin.site.register(Stream, StreamAdmin)
