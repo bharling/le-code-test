efdb6d0a37d042c8b395a118d488c70f0d33d676
Ben Harling <benjamin.harling@hotmail.com>
2015-01-11 12:45:12 +0000

    filter out deleted objects at view, pass deleted items test

--- testsite/stream/views.py
+++ testsite/stream/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from stream.models import Stream
 from django.http import HttpResponse
+from django.db.models import Q
 
 # Create your views here.
 
 
 def stream(request):
-    stream_items = Stream.objects.all()
+    stream_items = Stream.objects.filter( Q ( photo__deleted=False ) | Q  (tweet__deleted=False )   )
     return render(request, 'stream.html', {'stream':stream_items})
