b0072f0f9e79e0a74d33c6e31de73a06b6248a2f
Ben Harling <benjamin.harling@hotmail.com>
2015-01-11 10:50:22 +0000

    begin testing for new stream view

--- testsite/stream/tests.py
+++ testsite/stream/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class StreamViewTest(TestCase):
+    def test_can_visit_stream_page(self):
+        response = self.client.get('/stream')
+        self.assertEqual(response.status_code, '200', 'Could not load stream page')
