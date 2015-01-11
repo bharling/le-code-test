0d94c08fc294be95ff0ccba1fe83d253289856b6
Ben Harling <benjamin.harling@hotmail.com>
2015-01-11 10:30:01 +0000

    Link stream Items to Photos and Tweets. Add Validation to Stream Items to allow either a photo or tweet but not both

--- testsite/stream/models.py
+++ testsite/stream/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from items.models import PhotoItem, TweetItem
+from django.core.exceptions import ValidationError
 
 class Stream(models.Model):
     user = models.ForeignKey(User)
     created_at = models.DateTimeField()
+    tweet = models.ForeignKey(TweetItem, null=True, blank=True, related_name="streams")
+    photo = models.ForeignKey(PhotoItem, null=True, blank=True, related_name="streams")
+    
+    def clean(self):
+        if self.tweet is None and self.photo is None:
+            raise ValidationError('A Stream Item must have either a Tweet or a Photo')
+        if self.tweet and self.photo:
+            raise ValidationError('A stream Item cannot have both a Tweet AND a Photo')
+        
+    class Meta:
+        ordering = ['-created_at']
+            
