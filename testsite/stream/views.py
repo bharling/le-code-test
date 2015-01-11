da2354900ac44b79ef7efdf9b4d453081b37d016
Ben Harling <benjamin.harling@hotmail.com>
2015-01-11 11:09:46 +0000

    Basic view and template to make initial test pass

--- testsite/stream/views.py
+++ testsite/stream/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from stream.models import Stream
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def stream(request):
+    #return HttpResponse('ok')
+    stream_items = Stream.objects.filter(photo__deleted=False, tweet__deleted=False)
+    return render(request, 'stream.html', {stream:stream_items})
